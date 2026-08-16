f046417f92186bef9c1065d9e25caf0130a98746
shottzdeath-web <shottzdeath@gmail.com>
2026-08-16 14:24:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ sellerButton.addEventListener("click", function() {
     message.style.transform = "translateY(0)";
 
 });
+const menuToggle = document.getElementById("menu-toggle");
+const mainNav = document.getElementById("main-nav");
+
+menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("active");
+});
